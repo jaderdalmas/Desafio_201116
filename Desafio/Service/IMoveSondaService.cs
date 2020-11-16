b3c70afdd564ec849a95aea7c2fc31b2d2e4ddae
Jader Dalmas <jader@grayboxsolutions.com>
2020-11-16 15:47:06 -0300

    add move sonda service

---
+++ Desafio/Service/IMoveSondaService.cs
@@ -0,0 +1,26 @@
+﻿using Desafio.Model;
+using Desafio.ViewModel;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Desafio.Service
+{
+    public interface IMoveSondaService
+    {
+        /// <summary>
+        /// Move a list of sondas
+        /// </summary>
+        /// <param name="request">move sonda request</param>
+        /// <returns>move sonda response</returns>
+        MoveSondaResponse MoveSonda(MoveSondaRequest request);
+
+        /// <summary>
+        /// Move a Sonda
+        /// </summary>
+        /// <param name="sonda">sonda</param>
+        /// <param name="moves">moves</param>
+        /// <param name="limit">position limit</param>
+        /// <returns>sonda position</returns>
+        string MoveSonda(Sonda sonda, string moves = "", Position limit = null);
+    }
+}
---
+++ Desafio/Service/MoveSondaService.cs
@@ -0,0 +1,41 @@
+﻿using Desafio.Extension;
+using Desafio.Model;
+using Desafio.ViewModel;
+using System.Linq;
+
+namespace Desafio.Service
+{
+    public class MoveSondaService : IMoveSondaService
+    {
+        public MoveSondaService() { }
+
+        public MoveSondaResponse MoveSonda(MoveSondaRequest request)
+        {
+            var result = new MoveSondaResponse();
+
+            for (int i = 0; i < request.Sondas.Count(); i++)
+            {
+                result.Positions.Add(MoveSonda(request.Sondas.ElementAt(i), request.Moves.ElementAt(i)));
+            }
+
+            return result;
+        }
+
+        public string MoveSonda(Sonda sonda, string moves = "", Position limit = null)
+        {
+            if (sonda == null)
+                return string.Empty;
+            if (string.IsNullOrWhiteSpace(moves))
+                return sonda.ToString();
+
+            foreach (var move in moves)
+                sonda.Move(move);
+
+            if (sonda.Position.IsInvalid() ||
+                sonda.Position.OutOfLimit(limit))
+                return "Invalid";
+
+            return sonda.ToString();
+        }
+    }
+}
