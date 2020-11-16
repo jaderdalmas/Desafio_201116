2b88e4ec16e19e6ce3bb4084abc92528a61760a3
Jader Dalmas <jader@grayboxsolutions.com>
2020-11-16 18:04:02 -0300

    add startup functions

--- Desafio/Startup.cs
+++ Desafio/Startup.cs
@@ -1,3 +1,4 @@
+using Api.StartUp;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -6,26 +7,51 @@ using Microsoft.Extensions.Hosting;
 
 namespace Desafio
 {
+    /// <summary>
+    /// StartUp
+    /// </summary>
     public class Startup
     {
+        /// <summary>
+        /// Configuration
+        /// </summary>
+        public IConfiguration Configuration { get; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="configuration">Configuration</param>
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to add services to the container.
+        /// </summary>
+        /// <param name="services">Service Collection</param>
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddCompression();
+            services.AddHealthCheck();
+            services.AddSwagger();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app">Application Builder</param>
+        /// <param name="env">WebHost Environment</param>
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
                 app.UseDeveloperExceptionPage();
+
+            app.ConfigureCompression();
+            app.ConfigureHealthCheck();
+            app.ConfigureSwagger();
 
             app.UseHttpsRedirection();
             app.UseRouting();
---
+++ Desafio/Startup/Swagger.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System.Net;
+
+namespace Api.StartUp
+{
+    /// <summary>
+    /// Api Swagger
+    /// </summary>
+    public static class Swagger
+    {
+        /// <summary>
+        /// Register swagger
+        /// </summary>
+        /// <param name="services">Service collection</param>
+        public static void AddSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API" });
+
+                c.AddSecurityDefinition(AuthenticationSchemes.Basic.ToString(), new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = AuthenticationSchemes.Basic.ToString(),
+                    In = ParameterLocation.Header,
+                    Description = "Basic Authorization header using the Bearer scheme."
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {{
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = AuthenticationSchemes.Basic.ToString()
+                        }
+                    },
+                    new string[] {}
+                }});
+            });
+        }
+
+        /// <summary>
+        /// Configure Swagger
+        /// </summary>
+        /// <param name="app">App builder</param>
+        public static void ConfigureSwagger(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API");
+            });
+        }
+    }
+}
