37c90df8b4a342c66df179b58cb3be95d59c038d
Jader Dalmas <jader@grayboxsolutions.com>
2020-11-16 18:26:08 -0300

    add controller tests

---
+++ UnitTest/Controller/SondaControllerTest.cs
@@ -0,0 +1,55 @@
+using Desafio.Controllers;
+using Desafio.Model;
+using Desafio.Service;
+using Desafio.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Net;
+using Xunit;
+
+namespace UnitTest.Controller
+{
+    public class SondaControllerTest
+    {
+        [Fact]
+        public void MoveSonda_Null()
+        {
+            // Arrange
+            var controller = new SondaController(new MoveSondaService());
+            var request = new MoveSondaRequest();
+
+            // Act
+            var response = controller.Post(request);
+
+            // Assert
+            Assert.IsType<OkObjectResult>(response);
+            var result = response as OkObjectResult;
+            Assert.Equal(result.StatusCode, (int)HttpStatusCode.OK);
+            Assert.Null(result.Value);
+        }
+
+        [Fact]
+        public void MoveSonda()
+        {
+            // Arrange
+            var controller = new SondaController(new MoveSondaService());
+            var request = new MoveSondaRequest()
+            {
+                Sondas = new List<Sonda>() { new Sonda() },
+                Moves = new List<string>() { "RLMM" },
+                Limit = new Position(1, 1)
+            };
+
+            // Act
+            var response = controller.Post(request);
+
+            // Assert
+            Assert.IsType<OkObjectResult>(response);
+            var result = response as OkObjectResult;
+            Assert.Equal(result.StatusCode, (int)HttpStatusCode.OK);
+            Assert.NotNull(result.Value);
+            var value = result.Value as MoveSondaResponse;
+            Assert.Equal("Invalid", value.Positions[0]);
+        }
+    }
+}
