084525ae9a9c2837fe9096e2754e2911d15196cd
Jader Dalmas <jader@grayboxsolutions.com>
2020-11-16 15:03:54 -0300

    add sonda model

---
+++ Desafio/Model/Sonda.cs
@@ -0,0 +1,52 @@
+﻿using Desafio.Extension;
+
+namespace Desafio.Model
+{
+    public class Sonda
+    {
+        public Sonda(int x = 0, int y = 0, EFront front = EFront.N)
+        {
+            X = x;
+            Y = y;
+            Front = front;
+        }
+
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public EFront Front { get; private set; }
+
+        public void Move(char move)
+        {
+            if (move == 'M')
+                Move();
+            else
+                Front = Front.Move(move);
+        }
+
+        private void Move()
+        {
+            switch (Front)
+            {
+                case EFront.N:
+                    X++;
+                    break;
+                case EFront.W:
+                    Y--;
+                    break;
+                case EFront.S:
+                    X--;
+                    break;
+                case EFront.E:
+                    Y++;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public string GetPosition()
+        {
+            return $"{X} {Y} {Front}";
+        }
+    }
+}
