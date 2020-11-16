1c2ffaeec6b356d80901d1a4b5499d541d2c34ac
Jader Dalmas <jader@grayboxsolutions.com>
2020-11-16 15:04:11 -0300

    add sonda front extension

---
+++ Desafio/Extension/EFrontExtension.cs
@@ -0,0 +1,28 @@
+﻿using Desafio.Model;
+
+namespace Desafio.Extension
+{
+    public static class EFrontExtension
+    {
+        public static EFront Move(this EFront front, char move)
+        {
+            var position = (int)front;
+
+            switch (move)
+            {
+                case 'L':
+                    position += 1;
+                    if (position > 3) position -= 4;
+                    break;
+                case 'R':
+                    position -= 1;
+                    if (position < 0) position += 4;
+                    break;
+                default:
+                    break;
+            }
+
+            return (EFront)position;
+        }
+    }
+}
---
+++ Desafio/Model/EFront.cs
@@ -0,0 +1,25 @@
+﻿namespace Desafio.Model
+{
+    public enum EFront
+    {
+        /// <summary>
+        /// North
+        /// </summary>
+        N,
+
+        /// <summary>
+        /// West
+        /// </summary>
+        W,
+
+        /// <summary>
+        /// South
+        /// </summary>
+        S,
+
+        /// <summary>
+        /// East
+        /// </summary>
+        E
+    }
+}
