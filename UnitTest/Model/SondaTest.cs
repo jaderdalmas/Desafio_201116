62b4351e407946090a6186ef7580437945cbd8d1
Jader Dalmas <jader@grayboxsolutions.com>
2020-11-16 15:46:40 -0300

    fix sonda tests

--- UnitTest/Model/SondaTest.cs
+++ UnitTest/Model/SondaTest.cs
@@ -6,20 +6,37 @@ namespace UnitTest.Model
     public class SondaTest
     {
         [Theory]
-        [InlineData(EFront.N, "1 0 N")]
-        [InlineData(EFront.W, "0 -1 W")]
-        [InlineData(EFront.S, "-1 0 S")]
-        [InlineData(EFront.E, "0 1 E")]
-        public void Move_M(EFront front, string result)
+        [InlineData(EFront.N, "0 0 N")]
+        [InlineData(EFront.W, "0 0 W")]
+        [InlineData(EFront.S, "0 0 S")]
+        [InlineData(EFront.E, "0 0 E")]
+        public void Move_X(EFront front, string result)
         {
             // Arrange
             var sonda = new Sonda(front: front);
 
             // Act
+            sonda.Move('X');
+
+            // Assert
+            Assert.Equal(result, sonda.ToString());
+        }
+
+        [Theory]
+        [InlineData(EFront.N, "2 1 N")]
+        [InlineData(EFront.W, "1 0 W")]
+        [InlineData(EFront.S, "0 1 S")]
+        [InlineData(EFront.E, "1 2 E")]
+        public void Move_M(EFront front, string result)
+        {
+            // Arrange
+            var sonda = new Sonda(1, 1, front: front);
+
+            // Act
             sonda.Move('M');
 
             // Assert
-            Assert.Equal(result, sonda.GetPosition());
+            Assert.Equal(result, sonda.ToString());
         }
 
         [Theory]
@@ -36,7 +53,7 @@ namespace UnitTest.Model
             sonda.Move('L');
 
             // Assert
-            Assert.Equal(result, sonda.GetPosition());
+            Assert.Equal(result, sonda.ToString());
         }
 
         [Theory]
@@ -53,7 +70,7 @@ namespace UnitTest.Model
             sonda.Move('R');
 
             // Assert
-            Assert.Equal(result, sonda.GetPosition());
+            Assert.Equal(result, sonda.ToString());
         }
     }
 }
